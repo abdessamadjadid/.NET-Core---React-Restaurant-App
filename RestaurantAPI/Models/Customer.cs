981c7e1856c5570fbc8fa7acbdda2df340b2af4e
Abdessamad-Jadid <ajadid@miu.edu>
2022-03-29 00:13:35 -0500

    add Models and relations using EF Core

---
+++ RestaurantAPI/Models/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        private int CustomerId { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string CustomerName { get; set; }
+    }
+}
---
+++ RestaurantAPI/Models/OrderDetail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI.Models
+{
+    public class OrderDetail
+    {
+        [Key]
+        public int OrderDetailId { get; set; }
+
+        public long OrderMasterId { get; set; }
+        public OrderMaster orderMaster { get; set; }
+
+        public int FoodItemId { get; set; }
+        public FoodItem foodItem { get; set; }
+
+        public decimal FoodItemPrice { get; set; }
+
+        public int Quantity { get; set; }
+
+    }
+}
